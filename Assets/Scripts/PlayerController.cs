d203caaa643562057125d51f3b1aca80d1c5c830
TAN Juay Yong <jytan@ourkidslearningjourney.org>
2020-10-16 11:08:34 +0800

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+    }
+}
